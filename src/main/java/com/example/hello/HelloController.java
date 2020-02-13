bb8ab30bb352b10f344bb238544c1f32715bc45e
Carlos Briano <carlos.briano@softtek.com>
2020-02-12 18:05:13 -0600

    test

---
+++ src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")	
+	public ResponseEntity getRoles() {
+		return ResponseEntity.ok().body("Build by jenkins, carlos hi,2");
+	}
+	
+}
